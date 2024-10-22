16e87ed5d7b9f284d2b7f1c77f3c7ed31b2bcb60
LEESojung1998 <80635289+LEESojung1998@users.noreply.github.com>
2024-10-22 15:03:46 +0800

    Update Main_FIG2.R

--- Main_Figures_Code/Main_FIG2.R
+++ Main_Figures_Code/Main_FIG2.R
@@ -2,21 +2,11 @@ library("ggsci")
 library(ggpubr)
 library(tidyverse)
 
-## Figure 2-B - IFN g
-IFNg_GSE186169 <- read.delim("https://figshare.com/ndownloader/files/47070703")
-comparison <- c("Control","IFNg")
-ifng <- ggplot(IFNg_GSE186169, aes(x=Treatment, y=log2TPM, fill=Treatment)) + geom_quasirandom()+stat_compare_means(comparisons = comparison,label = "p.signif",label.y = 2)+ geom_boxplot(width=0.2) + facet_wrap(~HML2_1q22, scales='free')+theme(legend.text = element_text(size=10), legend.title = element_blank(),legend.position="bottom",plot.title = element_text(hjust = 0.5, colour = "black", size = 10),
-                                                                                                                                                                                                                                                    axis.title.x = element_text(colour = "black", size = 10), axis.ticks.x =  element_blank(),
-                                                                                                                                                                                                                                                    axis.text.x =  element_blank(),
-                                                                                                                                                                                                                                                    axis.title.y = element_text(colour = "black",size = 10),axis.text.y = element_text(colour = "black",size = 10),panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank(),axis.line = element_line(colour = "black")) 
-ifng <- ifng + scale_fill_manual(values = c("#374E55FF","#6A6599FF"))+ylab("log2 (TPM)")+theme(strip.background=element_rect(colour="black",fill="white"))+ scale_y_continuous(limits=c(0,4.3))
+My_Theme2 = theme(strip.background=element_rect(colour="white",fill="white"),legend.text = element_text(size=15), legend.title = element_blank(),legend.position="right",plot.title = element_text(hjust = 0.5, colour = "black", size = 15),
+                  axis.title.x = element_text(colour = "black", size = 15),
+                  axis.text.x = element_text(colour = "black",size = 15),
+                  axis.title.y = element_text(colour = "black",size = 15),axis.text.y = element_text(colour = "black",size = 15),panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank(),axis.line = element_line(colour = "black"))
 
-#### Figure 2-C - TNF a
-TNFa_GSE34329 <- read.delim("https://figshare.com/ndownloader/files/47070706")
-TNFa_GSE34329$Treatment <- gsub("_","\n", TNFa_GSE34329$Treatment, fixed=TRUE)
-
-tnfa <- ggplot(TNFa_GSE34329, aes(x=Treatment, y=log2TPM, fill=Treatment)) + geom_quasirandom()+stat_compare_means(label = "p.signif",ref.group = ".all.",  method="t.test")+ geom_boxplot(width=0.4) + facet_wrap(~HML2_1q22, scales='free')+theme(legend.text = element_text(size=10), legend.title = element_blank(),legend.position="bottom",plot.title = element_text(hjust = 0.5, colour = "black", size = 10),
-                                                                                                                                                                                                                                                    axis.title.x = element_text(colour = "black", size = 10), axis.ticks.x =  element_blank(),
-                                                                                                                                                                                                                                                    axis.text.x =  element_blank(),
-                                                                                                                                                                                                                                                    axis.title.y = element_text(colour = "black",size = 10),axis.text.y = element_text(colour = "black",size = 10),panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank(),axis.line = element_line(colour = "black")) 
-tnfa <- tnfa + scale_fill_jama()+ylab("log2 (TPM)")+theme(strip.background=element_rect(colour="black",fill="white"))+ scale_y_continuous(limits=c(0,3.3))
+## Fig 2B
+primary_cell <- read.delim("https://figshare.com/ndownloader/files/49913457")
+primary_cell <- ggplot(na.omit(overlap_final), aes(x=Primary_Cells, y=Mean_Junction_Read)) + geom_bar(stat="identity", fill="#79AF97FF", width=0.5)+ My_Theme2+xlab("Primary Cells with junction reads \n (N=11 out of total N=114)") +ylab("Mean (CPM)") + coord_flip()
