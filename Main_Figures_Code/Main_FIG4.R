0215df23c11b4df7cc1fc6048222ab5e992c3f2b
LEESojung1998 <80635289+LEESojung1998@users.noreply.github.com>
2024-10-22 15:09:45 +0800

    Update Main_FIG4.R

--- Main_Figures_Code/Main_FIG4.R
+++ Main_Figures_Code/Main_FIG4.R
@@ -4,9 +4,10 @@ My_Theme = theme(legend.text = element_text(size=10), legend.title = element_bla
                  axis.title.y = element_text(colour = "black",size = 10),axis.text.y = element_text(colour = "black",size = 10),panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank(),axis.line = element_line(colour = "black"))
 
 
+
 ## Differentially expressed genes between normal vs tumor
+## Fig 4C
 resOrdered_nvst_deseq <- read.delim("https://figshare.com/ndownloader/files/47070856")
-## Figure 4-B: H3K4me1 and H3K4me3 related genes
 TCGA_LUNG_histone <- read.delim("https://figshare.com/ndownloader/files/47070853")
 resOrdered_nvst_deseq$up <- "Up in\nNormal"
 resOrdered_nvst_deseq$up[resOrdered_nvst_deseq$log2FoldChange>0] <- "Up in\nTumor"
@@ -17,15 +18,21 @@ histone <- ggplot(data=resOrdered_nvst_deseq[rownames(resOrdered_nvst_deseq) %in
   geom_point() + theme_minimal() +geom_text_repel(color="black") +My_Theme+ scale_color_jama()
 
 
-## Figure 4-C: HSAEC and A549 expression
+#Fig 4D
 HSAEC_A549 <- read.delim("https://figshare.com/ndownloader/files/47070859")
 HSAEC_A549$batch <- HSAEC_A549$treatment
 HSAEC_A549$batch[HSAEC_A549$srr %in% c("SRR7687755","SRR7687756","SRR7687757")] <- "A549\nBatch1"
 HSAEC_A549$batch[HSAEC_A549$srr %in% c("SRR8245170","SRR8245171","SRR8245172")] <- "A549\nBatch2"
-hsaec_a549_long <- ggplot(HSAEC_A549[HSAEC_A549$name=="HML2_1q22.long",], aes(x=treatment, y=count, fill=treatment)) + geom_bar(position = position_dodge2(preserve = "single"), stat = "identity") + xlab("Cell line") + ylab("log2 (HML2_1q22.long TPM)") + My_Theme + scale_fill_manual(values=c("#6A6599FF","#80796BFF"))
+HSAEC_A549$treatment[HSAEC_A549$treatment=="HSAEC"]<-"hSAEC"
+hsaec_a549_long <- ggplot(HSAEC_A549[HSAEC_A549$name=="HML2_1q22.long",], aes(x=treatment, y=count, fill=treatment))+ geom_quasirandom(shape=21)+stat_summary(fun.y = mean,fun.ymin = mean,fun.ymax = mean, geom = "errorbar", width = 0.3,color="#e7ab03", size=1) + xlab("Cell line") + ylab("log2 (ERVK-7.long TPM)") +scale_y_continuous(limits=c(0,4.5))+ My_Theme + scale_fill_manual(values=c("#6A6599FF","#80796BFF"))
 
-## Figure 4-D: normal and tumor organoid expression
+
+## Fig 4E
 organoid <- read.delim("https://figshare.com/ndownloader/files/47070847")
 organoid$label <- gsub("_","\n", organoid$label, fixed=TRUE)
-organoid_graph <- ggplot(organoid[organoid$name=='HML2_1q22.long',], aes(x=label, y=count, fill=label)) + geom_boxplot(width=0.2)+xlab(" ") + scale_fill_manual(values=c("#B24745FF","#00A1D5FF"))+ylab("log2 (HML2_1q22.long TPM)")
+organoid_graph <- ggplot(organoid[organoid$name=='HML2_1q22.long',], aes(x=label, y=count, fill=label))+ geom_quasirandom(shape=21)+stat_summary(fun.y = mean,fun.ymin = mean,fun.ymax = mean, geom = "errorbar", width = 0.3,color="#e7ab03", size=1) +xlab(" ") + scale_fill_manual(values=c("#B24745FF","#00A1D5FF"))+ylab("log2 (HML2_1q22.long TPM)")
 organoid_graph <- organoid_graph+My_Theme
+
+## Fig 4F
+psc_day01535 <- read.delim("https://figshare.com/ndownloader/files/49913469")
+stem_cell <- ggplot(psc_day01535, aes(x=label, y=logval))+theme(strip.background=element_rect(colour="white",fill="white")) + My_Theme + scale_y_continuous(limits=c(0,0.8))+ geom_quasirandom(shape=21,fill="#374E55FF")+stat_summary(fun.y = mean,fun.ymin = mean,fun.ymax = mean, geom = "errorbar", width = 0.3,color="#e7ab03", size=1)+ ylab("ERVK-7.long") + xlab("")
