aeb6512f98998ae97664fffc094450ae4103920f
LEESojung1998 <80635289+LEESojung1998@users.noreply.github.com>
2024-10-22 15:04:12 +0800

    Update Main_FIG3.R

--- Main_Figures_Code/Main_FIG3.R
+++ Main_Figures_Code/Main_FIG3.R
@@ -6,12 +6,11 @@ My_Theme = theme(legend.text = element_text(size=10), legend.title = element_bla
 library(tidyverse)
 library(ggsci)
 
-## Load TCGA LUAD+LUSC log2 TPM expression matrix
 TCGA_LUNG_TPM <- read.delim("https://figshare.com/ndownloader/files/47070841")
 luad_pat <- read.delim("https://figshare.com/ndownloader/files/47070835", header=FALSE)
 luad_pat <- data.frame(t(luad_pat))
 
-## Figure 2-A : Boxplot
+##Fig 3C
 TCGA_LUNG_TPM_nec <- data.frame(melt(cbind(name=rownames(TCGA_LUNG_TPM)[1:4], TCGA_LUNG_TPM[1:4,])))
 TCGA_LUNG_TPM_nec$value <- log(as.numeric(TCGA_LUNG_TPM_nec$value)+1, base=2)
 TCGA_LUNG_TPM_nec$cohort <- "LUSC"
@@ -23,48 +22,31 @@ TCGA_LUNG_TPM_nec$label[TCGA_LUNG_TPM_nec$label=="01A"] <- "Tumor"
 TCGA_LUNG_TPM_nec$final_label <- paste(TCGA_LUNG_TPM_nec$cohort,TCGA_LUNG_TPM_nec$label, sep="\n")
 TCGA_LUNG_TPM_nec$final_label[str_detect(TCGA_LUNG_TPM_nec$final_label, "Normal")] <- "Normal"
 my_comparisons <- list(c("Normal", "LUAD\nTumor"), c("Normal", "LUSC\nTumor"), c("LUAD\nTumor", "LUSC\nTumor"))
+TCGA_LUNG_TPM_nec$name[TCGA_LUNG_TPM_nec$name == "HML2_1q22.long"] <- "ERVK-7.long"
+TCGA_LUNG_TPM_nec$name[TCGA_LUNG_TPM_nec$name == "HML2_1q22"] <- "ERVK-7"
+TCGA_LUNG_TPM_nec$name[TCGA_LUNG_TPM_nec$name == "HML2_1q22.short"] <- "ERVK-7.short"
 
-tcga <- ggplot(TCGA_LUNG_TPM_nec[str_detect(TCGA_LUNG_TPM_nec$name, "HML2_1q22"),], aes(x=final_label, y=value, fill=final_label)) + geom_quasirandom() + geom_boxplot(width=0.3) + facet_wrap(~name, scale="free")+ scale_fill_manual(values=c("#B24745FF","#00A1D5FF","#DF8F44FF"))
+tcga <- ggplot(TCGA_LUNG_TPM_nec[str_detect(TCGA_LUNG_TPM_nec$name, "ERVK-7"),], aes(x=final_label, y=value, fill=final_label))+ geom_quasirandom(shape=21, fill="white", alpha=0.1) + geom_boxplot(width=0.2,outlier.shape = NA)+ facet_wrap(~name)+ scale_fill_manual(values=c("#B24745FF","#00A1D5FF","#DF8F44FF"))
 tcga <- tcga + xlab("") + ylab("log2 (TPM)") + stat_compare_means(comparisons = my_comparisons, label = "p.signif",ref.group = ".all.", method="t.test")+theme(legend.text = element_text(size=10), legend.title = element_blank(),legend.position="bottom",plot.title = element_text(hjust = 0.5, colour = "black", size = 10),
                                                                                                                                                                axis.title.x = element_text(colour = "black", size = 10), axis.ticks.x =  element_blank(),
                                                                                                                                                                axis.text.x =  element_blank(),
                                                                                                                                                                axis.title.y = element_text(colour = "black",size = 10),axis.text.y = element_text(colour = "black",size = 10),panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank(),axis.line = element_line(colour = "black")) 
-tcga <- tcga + scale_y_continuous(limits=c(0,9.3))
+tcga <- tcga + scale_y_continuous(limits=c(0,9))+theme(strip.background=element_rect(colour="white",fill="white"))
 
-## Figure 2-B,C : Correlation
+## correlation
 TCGA_LUNG_TPM_cor <- data.frame(t(TCGA_LUNG_TPM[1:4,]))
 TCGA_LUNG_TPM_cor$final_label <- unlist(TCGA_LUNG_TPM_nec[match(rownames(TCGA_LUNG_TPM_cor), TCGA_LUNG_TPM_nec$variable),"final_label"])
 TCGA_LUNG_TPM_cor <- na.omit(TCGA_LUNG_TPM_cor)
 for(i in 1:4){
   TCGA_LUNG_TPM_cor[,i] <- log(as.numeric(TCGA_LUNG_TPM_cor[,i])+1, base=2)
 }
-cor1 <- ggplot(TCGA_LUNG_TPM_cor, aes(x=HML2_1q22.long, HML2_1q22, color=final_label)) + geom_point(color="black", alpha=0.5)+My_Theme+ scale_color_manual(values=c("#B24745FF","#00A1D5FF","#DF8F44FF"))
-cor1 <- cor1 + xlab("log2 (HML2_1q22.long TPM)") + ylab("log2 (HML2_1q22.short TPM)")+ facet_wrap(~final_label, scales="free")
+cor1 <- ggplot(TCGA_LUNG_TPM_cor, aes(x=HML2_1q22.long, HML2_1q22, color=final_label)) + geom_point(fill="black", color="black",alpha=0.1, shape=21)+My_Theme+ scale_color_manual(values=c("#B24745FF","#00A1D5FF","#DF8F44FF"))
+cor1 <- cor1 + xlab("log2 (ERVK-7.long TPM)") + ylab("log2 (ERVK-7 TPM)")+ facet_wrap(~final_label, scales="free")
 cor1 <- cor1 + stat_cor(method = "spearman", hjust=0.5, vjust=0, label.x = TCGA_LUNG_TPM_cor$HML2_1q22.long, 
-                        label.y=TCGA_LUNG_TPM_cor$HML2_1q22)+stat_smooth(method='lm', formula = 'y ~ x',size=3)+ scale_x_continuous(limits=c(0,7))+ scale_y_continuous(limits=c(0,8))
-cor2 <- ggplot(TCGA_LUNG_TPM_cor, aes(x=HML2_1q22.short, HML2_1q22, color=final_label)) + geom_point(color="black", alpha=0.5)+My_Theme+ scale_color_manual(values=c("#B24745FF","#00A1D5FF","#DF8F44FF"))
-cor2 <- cor2 + xlab("log2 (HML2_1q22.short TPM)") + ylab("log2 (HML2_1q22.short TPM)")+ facet_wrap(~final_label, scales="free")
+                        label.y=TCGA_LUNG_TPM_cor$HML2_1q22)+stat_smooth(method='lm', formula = 'y ~ x',size=0.8)+ scale_x_continuous(limits=c(0,7))+ scale_y_continuous(limits=c(0,8))
+cor2 <- ggplot(TCGA_LUNG_TPM_cor, aes(x=HML2_1q22.short, HML2_1q22, color=final_label)) + geom_point(fill="black",color="black" ,alpha=0.1, shape=21)+My_Theme+ scale_color_manual(values=c("#B24745FF","#00A1D5FF","#DF8F44FF"))
+cor2 <- cor2 + xlab("log2 (ERVK-7.short TPM)") + ylab("log2 (ERVK-7 TPM)")+ facet_wrap(~final_label, scales="free")
 cor2 <- cor2 + stat_cor(method = "spearman", hjust=0.5, vjust=0, label.x = TCGA_LUNG_TPM_cor$HML2_1q22.long, 
-                        label.y=TCGA_LUNG_TPM_cor$HML2_1q22)+stat_smooth(method='lm', formula = 'y ~ x',size=3)+ scale_x_continuous(limits=c(0,8.5))+ scale_y_continuous(limits=c(0,8))
-
-
-## Figure 2-D : GSVA
-tnf_nfkb <- read.delim("https://figshare.com/ndownloader/files/47070832")
-tnf_nfkb$label <- gsub("_","\n", tnf_nfkb$label, fixed=TRUE)
-
-gsva <- ggplot(tnf_nfkb, aes(x=label, y=GSVA, fill=label)) +facet_wrap(~pathway, scales="free")+scale_fill_manual(values=c("#B24745FF","#00A1D5FF"))+ geom_quasirandom() + geom_boxplot(width=0.2) + xlab("") + ylab("GSVA")+ scale_y_continuous(limits=c(-0.35,0.35))+theme(legend.text = element_text(size=13), legend.title = element_blank(),legend.position="bottom",plot.title = element_text(hjust = 0.5, colour = "black", size = 13),
-                                                                                                                                                                                                                                                                             axis.title.x = element_text(colour = "black", size = 13),
-                                                                                                                                                                                                                                                                             axis.text.x = element_text(colour = "black",size = 13),
-                                                                                                                                                                                                                                                                             axis.title.y = element_text(colour = "black",size = 13),axis.text.y = element_text(colour = "black",size = 13),panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.background = element_blank(),axis.line = element_line(colour = "black"))
-
-## Figure 2-E: GSEA
-tcga_gsea_raw <- read.delim("https://figshare.com/ndownloader/files/47070838")
-tcga_gsea_raw <- tcga_gsea_raw[order(abs(tcga_gsea_raw$normalizedEnrichmentScore),decreasing=TRUE),]
-tcga_gsea_nec <- tcga_gsea_raw[1:20,]
-tcga_gsea_nec <- tcga_gsea_nec[order((tcga_gsea_nec$normalizedEnrichmentScore)),]
-tcga_gsea_nec$geneSet <- factor(tcga_gsea_nec$geneSet, levels=tcga_gsea_nec$geneSet)
-tcga_gsea_nec$label <- "Neg"
-tcga_gsea_nec$label[tcga_gsea_nec$enrichmentScore > 0] <- "Pos"
-
-tcga_gsea <- ggplot(tcga_gsea_nec, aes(x=geneSet, y=normalizedEnrichmentScore, fill=label)) + geom_bar(stat="identity")+coord_flip()+scale_fill_manual(values=c("#79AF97FF","#80796BFF"))+My_Theme + ylab("Enrichment Score") + xlab("") 
-
+                        label.y=TCGA_LUNG_TPM_cor$HML2_1q22)+stat_smooth(method='lm', formula = 'y ~ x',size=0.8)+ scale_x_continuous(limits=c(0,8.5))+ scale_y_continuous(limits=c(0,8))
+cor1 <- cor1 + theme(strip.background=element_rect(colour="white",fill="white"))
+cor2 <- cor2 + theme(strip.background=element_rect(colour="white",fill="white"))
